f79658dea090f2be8facd861f7bec39d43cbde02
Sumit Guha <sumit03guha@gmail.com>
2025-01-08 16:11:11 +0530

    feat: add error handling and entry point validation in SimpleAccount contract

---
+++ src/Errors.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+error SimpleAccount__NotOwnerOrEntryPoint();
+error SimpleAccount__NotEntryPoint();
+error SimpleAccount__ExecutionFailed(bytes data);
+error HelperConfig__InvalidChainId(uint256 chainId);
--- src/SimpleAccount.sol
+++ src/SimpleAccount.sol
@@ -5,7 +5,9 @@ pragma solidity 0.8.28;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 import { IAccount } from "account-abstraction/contracts/interfaces/IAccount.sol";
+
 import { PackedUserOperation } from
     "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {
@@ -13,14 +15,25 @@ import {
     SIG_VALIDATION_FAILED
 } from "account-abstraction/contracts/core/Helpers.sol";
 
+import { SimpleAccount__NotOwnerOrEntryPoint } from "./Errors.sol";
+import { SimpleAccount__NotEntryPoint } from "./Errors.sol";
+import { SimpleAccount__ExecutionFailed } from "./Errors.sol";
+
 contract SimpleAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) { }
+    address private immutable _entryPoint;
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        _entryPoint = entryPoint;
+    }
+
+    receive() external payable { }
 
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     ) external returns (uint256 validationData) {
+        _requireEntryPointOrOwner();
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
         if (owner() != ECDSA.recover(ethSignedMessageHash, userOp.signature)) {
             return SIG_VALIDATION_FAILED;
@@ -30,12 +43,31 @@ contract SimpleAccount is IAccount, Ownable {
         return SIG_VALIDATION_SUCCESS;
     }
 
+    function execute(address dest, uint256 value, bytes memory functionData) external {
+        _requireEntryPointOrOwner();
+
+        (bool success, bytes memory data) = dest.call{ value: value }(functionData);
+        if (!success) revert SimpleAccount__ExecutionFailed(data);
+    }
+
     function _payPrefund(uint256 missingAccountFunds) internal virtual {
         if (missingAccountFunds != 0) {
             (bool success,) =
                 payable(msg.sender).call{ value: missingAccountFunds, gas: type(uint256).max }("");
             (success);
             //ignore failure (its EntryPoint's job to verify, not account.)
+        }
+    }
+
+    function _requireEntryPointOrOwner() private view {
+        if ((msg.sender != owner()) && (msg.sender != _entryPoint)) {
+            revert SimpleAccount__NotOwnerOrEntryPoint();
+        }
+    }
+
+    function _requireEntryPoint() private view {
+        if (msg.sender != _entryPoint) {
+            revert SimpleAccount__NotEntryPoint();
         }
     }
 }
