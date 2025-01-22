5926f68e36b603f2181dfcd8acf91dd0762e55f5
Sumit Guha <sumit03guha@gmail.com>
2025-01-22 23:10:33 +0530

    feat: implement transaction validation logic in SimpleAccountZkSync contract

--- src/zksync/SimpleAccountZkSync.sol
+++ src/zksync/SimpleAccountZkSync.sol
@@ -4,13 +4,23 @@ pragma solidity 0.8.28;
 
 import { IAccount } from "@era-contracts/contracts/interfaces/IAccount.sol";
 import { Transaction } from "@era-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import { SystemContractsCaller } from "@era-contracts/contracts/libraries/SystemContractsCaller.sol";
+import { NONCE_HOLDER_SYSTEM_CONTRACT } from "@era-contracts/contracts/Constants.sol";
+import { INonceHolder } from "@era-contracts/contracts/interfaces/INonceHolder.sol";
 
 contract SimpleAccountZkSync is IAccount {
     function validateTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
         Transaction calldata _transaction
-    ) external payable returns (bytes4 magic) { }
+    ) external payable returns (bytes4 magic) {
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, _transaction.nonce)
+        );
+    }
 
     function executeTransaction(
         bytes32 _txHash,
